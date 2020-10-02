ae330f9ea19f365e909bbe0be2c566f85e36dd7e
pandora024 <phin-phis@hotmail.com>
2020-10-02 19:33:30 +0700

    อัพเดต

--- components/Weather.js
+++ components/Weather.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'black',
         width: '100%',
         height: 250,
-        opacity: 0.7,
+        opacity: 0.4,
         alignItems: 'center',
     },
     medium: {
